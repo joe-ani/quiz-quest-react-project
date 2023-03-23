aa760661b8bfe8e2d63c765bfa2733dddc332cd8
Joseph Ani <anijoe688@gmail.com>
2023-03-24 00:49:55 +0100

    more fixes🪄

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import "./App.css";
 import Home from "./Home.js";
 import Load from "./Load.js";
 import Question from "./Question.js";
-import {
-  BrowserRouter as Router,    
-  Route,
-  Routes,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { useState } from "react";
 import Result from "./Result";
 import QuizQuestions from "./QuizQuestions.js";
 import { createContext } from "react";
 
-
 function App() {
   //  set a count down of 5 seconds after wards set showpage to true
   const [showPage, setShowPage] = useState(false);
-  const [time, setTime] = useState()
-  const [timeDuration, setTimeDuration] = useState()
-  const [questionCount, setQuestionCount] = useState()
-  
+  const [time, setTime] = useState();
+  const [timeDuration, setTimeDuration] = useState();
+  const [questionCount, setQuestionCount] = useState();
+  const [correctData, setCorrectData] = useState();
+  const [inCorrectData, setInCorrectData] = useState([]);
+  const [questionsData, setQuestionsData] = useState([]);
+
   // const AppContext = createContext()
 
   // Timer
@@ -40,11 +37,55 @@ function App() {
       <div className="App">
         <Routes>
           {!showPage && <Route path="/" element={<Load />} />}
-          {showPage && <Route path="/" element={<Home setQuestionCount={setQuestionCount} setTime={setTime} setTimeDuration={setTimeDuration} />} />}
-          <Route path="question" element={<Question time={time} timeDuration={timeDuration} setTime={setTime} setTimeDuration={setTimeDuration} setQuestionCount={setQuestionCount} questionCount={questionCount}/>  } />
-          <Route path="/home" element={<Home setQuestionCount={setQuestionCount} setTime={setTime} setTimeDuration={setTimeDuration}/> }/>
-          <Route path="/result" element={<Result/> }/>
-          <Route path="/test" element={<QuizQuestions/> }/>
+          {showPage && (
+            <Route
+              path="/"
+              element={
+                <Home
+                  setQuestionCount={setQuestionCount}
+                  setTime={setTime}
+                  setTimeDuration={setTimeDuration}
+                />
+              }
+            />
+          )}
+          <Route
+            path="question"
+            element={
+              <Question
+                time={time}
+                timeDuration={timeDuration}
+                setTime={setTime}
+                setTimeDuration={setTimeDuration}
+                setQuestionCount={setQuestionCount}
+                questionCount={questionCount}
+              />
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <Home
+                setQuestionCount={setQuestionCount}
+                setTime={setTime}
+                setTimeDuration={setTimeDuration}
+              />
+            }
+          />
+          <Route
+            path="/result"
+            element={
+              <Result
+                correctData={correctData}
+                setCorrectData={setCorrectData}
+                inCorrectData={inCorrectData}
+                setInCorrectData={setInCorrectData}
+                questionsData={questionsData}
+                setQuestionsData={setQuestionsData}
+              />
+            }
+          />
+          {/* <Route path="/test" element={<QuizQuestions/> }/> */}
         </Routes>
       </div>
     </Router>
