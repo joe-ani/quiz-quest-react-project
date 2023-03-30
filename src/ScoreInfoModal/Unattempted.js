c1ea28335f2b76f8a41aaeef01ef34a7475f290a
Joseph Ani <anijoe688@gmail.com>
2023-03-31 00:06:31 +0100

    code modification 🪄

--- src/ScoreInfoModal/Unattempted.js
+++ src/ScoreInfoModal/Unattempted.js
@@ -2,11 +2,33 @@ import React from "react";
 import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
 import "./Unattempted.css";
 
-function Unattempted() {
+function Unattempted({unattemptedInfo}) {
+
+  const displayUnattemptedData = unattemptedInfo.map((data, i) => {
+    return (
+      <div key={data} className="correct-main-container">
+        <div className="ques">
+          <div className="heading">Question {data.page}</div>
+          <div className="text">{data.question}</div>
+        </div>
+        <div className="line"></div>
+        <div className="answer">
+          <div className="heading">Answer</div>
+          <div className="text">{data.answer}</div>
+        </div>
+      </div>
+    );
+  });
+
   return <div className="unattempted">
     <div className="unattempted-cont">
-     <div className="text">Unattempted Questions</div>
     <InfoRoundedIcon/>
+     <div className="text">Unattempted Questions</div>
+    <div className="correct-scroll">DATA</div>
+      <div className="faders">
+        <div className="fade1"></div>
+        <div className="fade2"></div>
+      </div>
     </div>
   </div>;
 }
