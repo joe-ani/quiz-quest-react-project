7c07a39a484cf1d8bdbbcf3235299c5b0484c7c4
Joseph Ani <anijoe688@gmail.com>
2023-03-31 00:06:00 +0100

    code modification 🪄

--- src/ScoreInfoModal/InCorrectInfo.js
+++ src/ScoreInfoModal/InCorrectInfo.js
@@ -2,11 +2,16 @@ import React from "react";
 import KeyboardDoubleArrowUpRoundedIcon from "@mui/icons-material/KeyboardDoubleArrowUpRounded";
 import "./InCorrectInfo.css";
 
-function InCorrectInfo() {
+function InCorrectInfo({inCorrectInfo}) {
   return (
     <div className="incorrect-info-cont">
       <KeyboardDoubleArrowUpRoundedIcon className="arrow-down" />
-      
+      <div className="text">Questions You Failed</div>
+      <div className="correct-scroll">data</div>
+      <div className="faders">
+        <div className="fade1"></div>
+        <div className="fade2"></div>
+      </div>
     </div>
   );
 }
