94dcc2106b24246574e90f516da72a73ae2d293a
Joseph Ani <anijoe688@gmail.com>
2023-04-05 10:11:32 +0100

    made fixes🪄

--- src/TimesUpOverlay.js
+++ src/TimesUpOverlay.js
@@ -20,7 +20,7 @@ function TimesUpOverlay() {
           Retry
         </div>
         <div onClick={(e) => navigate("/result")} className="result-button">
-          <BarChartRoundedIcon className="navigate" />
+          <BarChartRoundedIcon style={{transform: "rotate(90deg)"}} className="navigate" />
           Results
         </div>
       </div>
