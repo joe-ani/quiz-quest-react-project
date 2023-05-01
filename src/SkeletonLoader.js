be718f84017b7706c0b934e705efea18f167042a
Joseph Ani <anijoe688@gmail.com>
2023-05-01 21:36:42 +0100

    made fixes🚀

--- src/SkeletonLoader.js
+++ src/SkeletonLoader.js
@@ -7,34 +7,11 @@ function SkeletonLoader() {
       <div className="question-container2">
         <div className="question-cont">
           <h2></h2>
-
-          <p
-            style={{
-              margin: "20px",
-              marginTop: "50px",
-              textAlign: "center",
-            }}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam,
-            officiis?
-          </p>
-          <p
-            style={{
-              position: "absolute",
-              margin: "20px",
-              opacity: "1",
-              top: "25px",
-              marginTop: "50px",
-              textAlign: "center",
-              width: "50%",
-              height: "15px",
-            }}
-          ></p>
         </div>
+          <p></p>
         <div className="option-cont">
           <h2></h2>
           <div className="option-grid">
-            <div className="option"> </div>
             <div className="option"></div>
             <div className="option"></div>
             <div className="option"></div>
